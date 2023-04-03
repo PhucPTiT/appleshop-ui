b1422bba16131cdfc6c6ec74721d1dd7420d7721
PhucPTiT <phucndah2002@gmail.com>
2023-04-03 17:47:50 +0700

    bug

--- src/pages/admin/products/index.js
+++ src/pages/admin/products/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import { FaBars, FaArchive, FaUser, FaCommentDots, FaLightbulb } from 'react-icons/fa';
 
-import Form from '~/components/Form';
+import SideBarItem from '~/components/ItemSidebar';
 import styles from './Products.module.scss';
-import Header from '~/components/Header';
+import Input from '~/components/Input';
 
 const cx = classNames.bind(styles);
 
 function Products() {
-    const fields = [
-        { type: 'text', name: 'name', placeholder: 'Enter your category' },
-        { type: 'text', name: 'code', placeholder: 'Enter your category code', class: 'ip-categoryCode' },
+    const ListItems = [
+        { icon: FaLightbulb, name: 'Category', to: '/' },
+        { icon: FaArchive, name: 'Product', to: '/products' },
+        { icon: FaUser, name: 'User', to: '' },
+        { icon: FaCommentDots, name: 'Comment', to: '/' },
     ];
+
     return (
         <div className={cx('test')}>
-            <Header />
-            <Form fields={fields} className="category" />
+            <div className={cx('sidebar')}>
+                <FaBars className={cx('sidebar-divider')} />
+                {ListItems.map((item, index) => {
+                    return (
+                        <SideBarItem iconLeft={item.icon} to={item.to}>
+                            {item.name}
+                        </SideBarItem>
+                    );
+                })}
+            </div>
         </div>
     );
 }
