e3fc020d6103d94e1bda03d1a19b562a47b68e51
PhucPTiT <phucndah2002@gmail.com>
2023-04-11 11:35:47 +0700

    upload header footer ui

--- src/layouts/DefaultLayout/Footer/index.js
+++ src/layouts/DefaultLayout/Footer/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Footer.module.scss';
 import { FaBox, FaCrown, FaHandsHelping, FaShieldAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -30,7 +31,8 @@ const KeySelling = listKey.map((key, index) => {
     const { icon, title, desc } = key;
     return (
         <div className={cx('key-selling-item')}>
-            {icon}
+            <div className={cx('icon')}>{icon}</div>
+
             <div className={cx('wrap-content')}>
                 <span className={cx('title')}>{title}</span>
                 <span className={cx('desc')}>{desc}</span>
@@ -38,40 +40,111 @@ const KeySelling = listKey.map((key, index) => {
         </div>
     );
 });
+
 function Footer() {
     return (
         <div className={cx('footer')}>
-            <div className={cx('key-selling')}>{KeySelling}</div>
+            <div className={cx('key-selling')}>
+                <div className={cx('container')}>{KeySelling}</div>
+            </div>
             <div className={cx('footer-middle')}>
-                <div className={cx('footer-info')}>
-                    <div className={cx('hotline')}>
-                        <div className={cx('item')}>
-                            <div className={cx('text')}>Giao hàng miễn phí</div>
-                            <a className={cx('phone')} href="tel:18006601">
-                                1800 6601
-                            </a>
+                <div className={cx('container')}>
+                    <div className={cx('footer-content')}>
+                        <div className={cx('footer-info')}>
+                            <div className={cx('hotline')}>
+                                <div className={cx('item')}>
+                                    <div className={cx('text')}>Giao hàng miễn phí</div>
+                                    <a className={cx('phone')} href="tel:18006601">
+                                        1800 6601
+                                    </a>
+                                </div>
+                                <div className={cx('item')}>
+                                    <div className={cx('text')}>Chăm sóc khách hàng</div>
+                                    <a className={cx('phone')} href="tel:18006616">
+                                        1800 6616
+                                    </a>
+                                </div>
+                            </div>
+                            <div className={cx('service')}>
+                                <div className={cx('text')}> Dịch vụ và hỗ trợ</div>
+                                <ul>
+                                    <li>HCM: 121 Lê Lợi, Phường Bến Nghé, Quận 1, TP.HCM</li>
+                                    <li>Hà Nội: 92 Hai Bà Trưng, P. Cửa Nam, Q. Hoàn Kiếm, TP. Hà Nội</li>
+                                    <li>Đà Nẵng: 240 Hoàng Diệu, Q. Hải Châu, TP. Đà Nẵng</li>
+                                    <li>Cần Thơ: 52 - 54 - 56 Đường 30/4, P. An Phú, Q. Ninh Kiều, Cần Thơ</li>
+                                </ul>
+                            </div>
+                            <div className={cx('img')}>
+                                <img src={require('~/assets/image/lg-bct.png')} alt="item-footer" />
+                            </div>
                         </div>
-                        <div className={cx('item')}>
-                            <div className={cx('text')}>Chăm sóc khách hàng</div>
-                            <a className={cx('phone')} href="tel:18006616">
-                                1800 6616
-                            </a>
+                        <div className={cx('footer-category')}>
+                            <div className={cx('category-item')}>
+                                <div className={cx('title')}>Sản phẩm</div>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Iphone</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Ipad</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Mac</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Apple Watch</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Phụ Kiện</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={cx('category-item')}>
+                                <div className={cx('title')}>Chính sách</div>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Chính sách bảo mật</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Chính sách đổi trả sản phẩm</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Chính sách bảo hành</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Chính sách giao nhận</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={cx('category-item')}>
+                                <div className={cx('title')}>Về chúng tôi</div>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Giới thiệu</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Tin tức thủ thuật</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Hệ thống cửa hàng</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Câu hỏi thường gặp</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Hướng dẫn mua hàng</Link>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className={cx('service')}>
-                        <div className={cx('text')}> Dịch vụ và hỗ trợ</div>
-                        <ul>
-                            <li>HCM: 121 Lê Lợi, Phường Bến Nghé, Quận 1, TP.HCM</li>
-                            <li>Hà Nội: 92 Hai Bà Trưng, P. Cửa Nam, Q. Hoàn Kiếm, TP. Hà Nội</li>
-                            <li>Đà Nẵng: 240 Hoàng Diệu, Q. Hải Châu, TP. Đà Nẵng</li>
-                            <li>Cần Thơ: 52 - 54 - 56 Đường 30/4, P. An Phú, Q. Ninh Kiều, Cần Thơ</li>
-                        </ul>
-                    </div>
-                    <div className={cx('img')}>
-                        <img src={require('~/assets/image/lg-bct.png')} alt="item-footer" />
                     </div>
                 </div>
-                <div className={cx('footer-category')}></div>
+            </div>
+            <div className={cx('footer-bottom')}>
+                <p>
+                    Apple, the Apple logo are trademark of Apple inc, registered in the US, and other countries
+                    Copyright 2007 - 2022 Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số. GPĐKKD số 031160988.
+                </p>
             </div>
         </div>
     );
--- src/layouts/DefaultLayout/Header/index.js
+++ src/layouts/DefaultLayout/Header/index.js
@@ -3,6 +3,7 @@ import { FaSistrix, FaUser, FaShoppingCart } from 'react-icons/fa';
 
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -11,10 +12,10 @@ function Header() {
         <div className={cx('header')}>
             <div className={cx('container')}>
                 <div className={cx('wrap-header')}>
-                    <div className={cx('logo')}>
+                    <Link to="/" className={cx('logo')}>
                         <p>P.Studio</p>
                         <img src={require('~/assets/image/copyright_logo.png')} alt={'myimage'} />
-                    </div>
+                    </Link>
                     <div className={cx('search')}>
                         <FaSistrix />
                         <input type="text" placeholder="Bạn đang tìm kiếm sản phẩm" />
